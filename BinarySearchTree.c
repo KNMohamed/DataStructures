fb5483b9e0694020ba6e13fa34da2e66f6894872
kmohamed <kmohamed@mail.uoguelph.ca>
2016-05-03 23:42:44 -0400

    changes

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum{InOrder,PostOrder,PreOrder} TraversalOrder;
+
 struct tree_node{
 	struct tree_node *left,*right;
 	int value;
@@ -18,8 +20,8 @@ void tree_insert(struct tree *t, struct tree_node *i){
 
 }
 
-tree_node new_node(int v){
-	tree_node *temp = (tree_node*)malloc(sizeof(*temp));
+struct tree_node* new_node(int v){
+	struct tree_node *temp = (struct tree_node*)malloc(sizeof(*temp));
 	if(temp == NULL){ printf("Unable to allocate Memory!\n"); exit(0); }
 	temp->value = v;	
 	temp->left = NULL;
@@ -35,8 +37,31 @@ void tree_erase(struct tree_node *t){
 	free(t);
 }
 
-void tree_clear(struct tree){
-	tree_erase(tree->root);
-	tree->num_nodes = 0;
+void tree_clear(struct tree *t){
+	tree_erase(t->root);
+	t->num_nodes = 0;
 }
 
+void traverse(struct tree_node *t, TraversalOrder TO){
+	if(t != NULL){
+		if(TO == PreOrder){
+			printf("%d",t->value);
+			traverse(t->left,PreOrder);
+			traverse(t->right,PreOrder);
+		}else if(TO == InOrder){
+			printf("(");
+			traverse(t->left,InOrder);
+			printf("%d",t->value);
+			traverse(t->right,InOrder);
+			printf(")");
+		}else if(TO == PostOrder){
+			traverse(t->left,PostOrder);
+			traverse(t->right,PostOrder);
+			printf("%d",t->value);
+		}
+	}
+}
+
+int main(int argc, char *argv[]){
+	return 0;
+}
